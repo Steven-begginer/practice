54ac0291d2fdfd5e342e406f974e15e4f129d09e
practice <stevenyounick@gmail.com>
2025-12-25 16:47:07 +0800

    change the for loop to while loop method

--- practice.py
+++ practice.py
@@ -1,14 +1,16 @@
 # algorithm : linear search 
 arr = [1,3,4,6,10,11,31]
-guessed_value = int(input()) # loop 7 times, from 0,1,2,3,4,5,6
-for i in range(len(arr)):
+guessed_value = int(input()) # loop 7 times, from 0,1,2,3,4,5,6. The law is the arthimetic relationship between the indices and number of times.
+i = 0
+while i < len(arr): 
     if arr[i] == guessed_value:
         print("you find out the value")
         break
     elif arr[i] != guessed_value:
         if i < len(arr):
             print("you do not find out the value and continue to open the next box")
-            if i == len(arr) -1:
+            i += 1
+            if i == len(arr):
                 print("value not exist")
     
     
