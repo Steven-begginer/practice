07a35c7a67f73563220232231ba71d4cb57dfbab
practice <stevenyounick@gmail.com>
2025-12-25 19:27:30 +0800

    new knowledge

---
+++ practice2.py
@@ -0,0 +1,3 @@
+for i in range(5):
+    print(i)
+print("Do not forget the location to exit the for loop") # where it is aligned with the "for" location, meaning jump out of the whole process, we can choose to print or not.
