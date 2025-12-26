da2550f7c89684012d1b2f832592c6ee050a8035
practice <stevenyounick@gmail.com>
2025-12-26 10:41:00 +0800

    bubble_sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -2,10 +2,11 @@ arr = [7,5,3,1]
 for i in range(len(arr)-1): 
     for i in range(len(arr)-1):
         if arr[i] > arr[i+1]:
-            arr[i], arr[i+1] = arr[i+1], arr[i]
+            arr[i], arr[i+1] = arr[i+1], arr[i] # "=" can change the function label, "==" is the condition label.
         else:
             continue
 print(arr)
+
 
 
         
