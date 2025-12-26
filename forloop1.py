e1dd4a90f99cb92ad1f00ff7966996a8f43a80c3
practice <stevenyounick@gmail.com>
2025-12-26 10:27:05 +0800

    potential rules for for loop

--- forloop1.py
+++ forloop1.py
@@ -2,3 +2,4 @@ arr = [1,2,3,4]
 for i in range(len(arr)):
     for j in range(i+5, len(arr)):
         print(arr[j])
+# looking for the result, we can see when the for loop has the maximal range or called stop, it can not exceed to the stop even if the start value is more than the stop value.
