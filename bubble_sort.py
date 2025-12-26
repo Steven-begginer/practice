fcec726aa5ef5d34082743b0e2573ba94c493d93
practice <stevenyounick@gmail.com>
2025-12-26 11:35:16 +0800

    optimize my algorithm when the best case

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,10 +1,15 @@
 a = [7,5,3,1]
 for i in range(len(a)-1): 
+    swap = False 
     for i in range(len(a)-1):
         if a[i] > a[i+1]:
             a[i], a[i+1] = a[i+1], a[i] # "=" can change the function label, "==" is the condition label.
+            swap = True #no swap is false
         else:
             continue
+    if swap == False: #no swap is True. we can use "if not swap:" instead of "if swap == False". The location is to jump out of internal loop.
+        break
+
 print(a)
 
 
