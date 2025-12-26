efadbcf37e91e98175e3359e83c0263c5949bcea
practice <stevenyounick@gmail.com>
2025-12-26 10:39:50 +0800

    bubble_sort

---
+++ bubble_sort.py
@@ -0,0 +1,11 @@
+arr = [7,5,3,1]
+for i in range(len(arr)-1): 
+    for i in range(len(arr)-1):
+        if arr[i] > arr[i+1]:
+            arr[i], arr[i+1] = arr[i+1], arr[i]
+        else:
+            continue
+print(arr)
+
+
+        
